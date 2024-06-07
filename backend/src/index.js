a6cf61389dda2e23391326368a20c5089b6cbc41
filip.galetic@gmail.com <filip.galetic@gmail.com>
2024-06-07 16:27:39 +0200

    chore: Update .gitignore to exclude .package-lock.json file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const { connectDB } = require("./mongo/connection");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-console.log('Server is running on http://localhost:3000');
-});
+connectDB().then(() => console.log("Connected to database!"))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
---
+++ backend/src/models/Order.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  total: Number,
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
