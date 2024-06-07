c287fe080b3111f22e44039cf9fb9f8be192e1a4
filip.galetic@gmail.com <filip.galetic@gmail.com>
2024-06-07 17:57:20 +0200

    chore: Update backend package.json and index.js files to use ES modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const { connectDB } = require("./mongo/connection");
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// Connect to MongoDB
+const mongoURI = process.env.MONGODB_URI;
+if (!mongoURI) {
+  console.error('MONGODB_URI is not defined in the environment variables');
+  process.exit(1);
+}
 
-connectDB().then(() => console.log("Connected to database!"))
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
